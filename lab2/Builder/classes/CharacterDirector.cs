ecfabd2aba214bb0dacd54181f82fd9e581446ef
Serhiiko Vasyl <ipzk241_svm@student.ztu.edu.ua>
2025-03-07 00:12:29 +0200

    builder changes

--- lab2/Builder/classes/CharacterDirector.cs
+++ lab2/Builder/classes/CharacterDirector.cs
@@ -7,75 +7,84 @@ using System.Threading.Tasks;
 
 namespace Builder.classes
 {
-	internal class CharacterDirector
-	{
+    internal class CharacterDirector
+    {
+        ICharacterBuilder _builder;
 
-		public ICharacter NoobHero(ICharacterBuilder builder)
-		{
-			return builder
-				.SetName("Пригодник")
-				.SetHeight(172)
-				.SetBuild("Стрункий")
-				.SetHairColor("Коричневий")
-				.SetEyeColor("Синій")
-				.SetClothing("Базова броня")
-				.SetActiveWeapon("Палка")
-				.AddInventoryItem("Дерев'яний меч")
-				.AddInventoryItem("Зілля здоров'я")
-				.AddDeed("Врятував курку від лисиці")
-				.Build();
-		}
-		public ICharacter ProHero(ICharacterBuilder builder)
-		{
-			return builder
-				.SetName("Супер Рицар")
-				.SetHeight(188)
-				.SetBuild("Мускулистий")
-				.SetHairColor("Чорний")
-				.SetEyeColor("Зелений")
-				.SetClothing("Елітна броня")
-				.SetActiveWeapon("Меч ворона")
-				.AddInventoryItem("Магічний меч")
-				.AddInventoryItem("Зілля сили")
-				.AddInventoryItem("Щит дракона")
-				.AddDeed("Переміг дракона")
-				.AddDeed("Врятував королівство")
-				.AddDeed("Почав розводити курей")
-				.AddDeed("Знайшов скарб")
-				.Build();
-		}
-		public ICharacter MobEnemy(ICharacterBuilder builder)
-		{
-			return builder
-				.SetName("Гоблін")
-				.SetHeight(155)
-				.SetBuild("Середній")
-				.SetHairColor("Сірий")
-				.SetEyeColor("Червоний")
-				.SetClothing("Проста броня")
-				.SetActiveWeapon("Звичайний меч")
-				.AddInventoryItem("Звичайний меч")
-				.AddInventoryItem("Зілля здоров'я")
-				.AddDeed("Вдарив курку")
-				.Build();
-		}
-		public ICharacter BossEnemy(ICharacterBuilder builder)
-		{
-			return builder
-				.SetName("Бос")
-				.SetHeight(200)
-				.SetBuild("Гігантський")
-				.SetHairColor("Чорний")
-				.SetEyeColor("Червоний")
-				.SetClothing("Магічна броня")
-				.SetActiveWeapon("Меч зла")
-				.AddInventoryItem("Меч зла")
-				.AddInventoryItem("Зілля безсмертя")
-				.AddInventoryItem("Щит темряви")
-				.AddDeed("Знищив село")
-				.AddDeed("Вдарив 2 курки")
-				.AddDeed("Спалив курятник")
-				.Build();
-		}
-	}
+        public CharacterDirector(ICharacterBuilder builder)
+        {
+            _builder = builder;
+        }
+
+        public void ChangeBuilder(ICharacterBuilder builder)
+        { _builder = builder; }
+
+        public ICharacter NoobHero()
+        {
+            return _builder
+                .SetName("Пригодник")
+                .SetHeight(172)
+                .SetBuild("Стрункий")
+                .SetHairColor("Коричневий")
+                .SetEyeColor("Синій")
+                .SetClothing("Базова броня")
+                .SetActiveWeapon("Палка")
+                .AddInventoryItem("Дерев'яний меч")
+                .AddInventoryItem("Зілля здоров'я")
+                .AddDeed("Врятував курку від лисиці")
+                .Build();
+        }
+        public ICharacter ProHero()
+        {
+            return _builder
+                .SetName("Супер Рицар")
+                .SetHeight(188)
+                .SetBuild("Мускулистий")
+                .SetHairColor("Чорний")
+                .SetEyeColor("Зелений")
+                .SetClothing("Елітна броня")
+                .SetActiveWeapon("Меч ворона")
+                .AddInventoryItem("Магічний меч")
+                .AddInventoryItem("Зілля сили")
+                .AddInventoryItem("Щит дракона")
+                .AddDeed("Переміг дракона")
+                .AddDeed("Врятував королівство")
+                .AddDeed("Почав розводити курей")
+                .AddDeed("Знайшов скарб")
+                .Build();
+        }
+        public ICharacter MobEnemy()
+        {
+            return _builder
+                .SetName("Гоблін")
+                .SetHeight(155)
+                .SetBuild("Середній")
+                .SetHairColor("Сірий")
+                .SetEyeColor("Червоний")
+                .SetClothing("Проста броня")
+                .SetActiveWeapon("Звичайний меч")
+                .AddInventoryItem("Звичайний меч")
+                .AddInventoryItem("Зілля здоров'я")
+                .AddDeed("Вдарив курку")
+                .Build();
+        }
+        public ICharacter BossEnemy()
+        {
+            return _builder
+                .SetName("Бос")
+                .SetHeight(200)
+                .SetBuild("Гігантський")
+                .SetHairColor("Чорний")
+                .SetEyeColor("Червоний")
+                .SetClothing("Магічна броня")
+                .SetActiveWeapon("Меч зла")
+                .AddInventoryItem("Меч зла")
+                .AddInventoryItem("Зілля безсмертя")
+                .AddInventoryItem("Щит темряви")
+                .AddDeed("Знищив село")
+                .AddDeed("Вдарив 2 курки")
+                .AddDeed("Спалив курятник")
+                .Build();
+        }
+    }
 }
--- lab2/Builder/obj/Debug/net8.0/Builder.AssemblyInfo.cs
+++ lab2/Builder/obj/Debug/net8.0/Builder.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Builder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+364a9aa5bdc1a974ab89de62c2de2d49e63e1b41")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a08f76bfb61fa4da1f40c20f87156e80bac1491")]
 [assembly: System.Reflection.AssemblyProductAttribute("Builder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Builder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
